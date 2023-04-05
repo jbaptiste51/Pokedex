cce1faf9395b39e9eebd3e16c8b7e0b10cbcd83d
jean-baptiste NOIRAUD <jean.noiraud@gmail.com>
2023-04-05 11:52:02 +0200

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const App = () => {
 
   return (
     <div className="App">
-      <PokemonCard/>
+      <PokemonCard pokemon = {pokemonList[0]}/>
     </div>
   )
 }
